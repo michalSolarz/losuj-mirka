2f4aec6540d26461d2e427bcc7435048c7354f90
Michał Solarz <michalsolarz83@gmail.com>
2014-12-29 15:33:55 +0100

    Initial commit

--- module/Lottery/config/module.config.php
+++ module/Lottery/config/module.config.php
@@ -1,3 +1,34 @@
 <?php
 return array(
+    'controllers' => array(
+        'invokables' => array(
+            'Lottery\Controller\Main' => 'Lottery\Controller\MainController',
+        ),
+    ),
+
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'main' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/main[/][:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Lottery\Controller\Main',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
+
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'lottery' => __DIR__ . '/../view',
+        ),
+    ),
 );
---
+++ module/Lottery/src/Lottery/Controller/MainController.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bezimienny
+ * Date: 29.12.14
+ * Time: 15:18
+ */
+
+namespace Lottery\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class MainController extends  AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+}
