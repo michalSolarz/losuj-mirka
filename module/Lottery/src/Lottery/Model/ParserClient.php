5934892eace2658e98102ad8afecd9903bd3d0d1
Michał Solarz <michalsolarz83@gmail.com>
2014-12-30 09:55:09 +0100

    Losuj Mirka v.0.0.1
    - Added parsing UpVoters
    - UpVoters split

---
+++ module/Lottery/src/Lottery/Model/ParserClient.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bezimienny
+ * Date: 30.12.14
+ * Time: 00:26
+ */
+
+namespace Lottery\Model;
+
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Http\Client as HttpClient;
+
+class ParserClient extends AbstractActionController
+{
+    public function getContent($ajaxUrl)
+    {
+        $client = new HttpClient();
+        $client->setAdapter('Zend\Http\Client\Adapter\Curl');
+
+        $response = $this->getResponse();
+        //set content-type
+        /** @noinspection PhpUndefinedMethodInspection */
+        $response->getHeaders()->addHeaderLine('content-type', 'text/html; charset=utf-8');
+
+        $client->setUri($ajaxUrl);
+        $result = $client->send();
+        //content of the web
+        $body = $result->getBody();
+
+        return $body;
+    }
+}
---
+++ module/Lottery/src/Lottery/Model/UpVoters.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bezimienny
+ * Date: 29.12.14
+ * Time: 22:34
+ */
+
+namespace Lottery\Model;
+
+
+class UpVoters
+{
+    private $ajaxBaseLink = "http://www.wykop.pl/ajax2/";
+    private $requestType = "upvoters";
+
+    private $upVoters;
+
+    private $lastUpVoter;
+    private $lastUpVoterPosition;
+
+    private $activeUpVoters;
+    private $inactiveUpVoters;
+
+    function __construct($link, $lastUpVoter)
+    {
+        $this->setUpVoters($link);
+        $this->setUpLastVoter($lastUpVoter);
+        $this->splitUpVoters();
+    }
+
+    private function setUpVoters($link)
+    {
+        if (!isset($this->upVoters)) {
+            $parser = new Parser($link, $this->ajaxBaseLink, $this->requestType);
+            if (($this->upVoters = $parser->parseUpVoters()) == false)
+                throw new \Exception("Failed to parse UpVoters");
+        }
+    }
+
+    private function setUpLastVoter($lastUpVoter)
+    {
+        if ($lastUpVoterPosition = array_search($lastUpVoter, $this->upVoters)) {
+            $this->lastUpVoter = $lastUpVoter;
+            $this->lastUpVoterPosition = $lastUpVoterPosition;
+        }
+    }
+
+    private function splitUpVoters()
+    {
+        if (isset($this->upVoters) && isset($this->lastUpVoterPosition)) {
+            $this->activeUpVoters = $this->upVoters;
+            $this->inactiveUpVoters = array_splice($this->activeUpVoters, ($this->lastUpVoterPosition + 1));
+        } else
+            throw new \Exception("Set UpVoters and LastUpVoter first.");
+    }
+
+    public function getActiveUpVoters()
+    {
+        if (isset($this->activeUpVoters))
+            return $this->activeUpVoters;
+        else
+            throw new \Exception("Set UpVoters and ActiveUpVoters first.");
+    }
+
+    public function getInactiveUpVoters()
+    {
+        if (isset($this->inactiveUpVoters))
+            return $this->inactiveUpVoters;
+        else
+            throw new \Exception("Set UpVoters and ActiveUpVoters first.");
+    }
+
+    public function getUpVoters()
+    {
+        if (isset($this->upVoters))
+            return $this->upVoters;
+        else
+            throw new \Exception("Set UpVoters first.");
+    }
+}
